9dd6fdf85cb658cc5693eeaee499ccf66f89b8e2
Eizel Landicho <eizelgrace@gmail.com>
2014-01-31 23:33:16 +0800

    Initial Commit.

---
+++ home_footer.php
@@ -0,0 +1,18 @@
+     <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>© 2013 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="dist/js/jquery-1.js"></script>
+    <script src="dist/js/bootstrap.js"></script>
+    <script src="dist/js/holder.js"></script>
+  
+
+</body></html>
